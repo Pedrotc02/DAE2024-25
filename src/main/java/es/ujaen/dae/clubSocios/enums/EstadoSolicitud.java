adc7ee87e7485ffab7025122461218b6584a37d8
tomasj10 <tj1072003@gmail.com>
2024-10-16 11:39:23 +0200

    Estado Solicitud EN_ESPERA

--- src/main/java/es/ujaen/dae/clubSocios/enums/EstadoSolicitud.java
+++ src/main/java/es/ujaen/dae/clubSocios/enums/EstadoSolicitud.java
@@ -4,11 +4,12 @@ package es.ujaen.dae.clubSocios.enums;
  * PENDIENTE: no se ha asignado ninguna plaza de la solicitud, nisiquiera la del socio (ya sea porque su estado de cuota sea pendiente u otro motivo)
  * PARCIAL: se ha asignado la plaza del socio. Estas solicitudes tendrán prioridad sobre las solicitudes PENDIENTES.
  * CERRADA: se han asignado todas las plazas exitosamente de la solicitud.
- * CANCELADA: la solicitud no se ha aceptado (no hay plazas en la actividad, por ejemplo).
+ * EN_ESPERA: la solicitud no se ha aceptado, se deja para cuando se acabe el período de inscripción y lo haga la dirección(no hay plazas en la actividad, por ejemplo).
  */
+
 public enum EstadoSolicitud {
     PENDIENTE,
     PARCIAL,
     CERRADA,
-    CANCELADA;
+    EN_ESPERA;
 }
