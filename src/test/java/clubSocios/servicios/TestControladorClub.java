dcebb90b102a87dad3ce913d159a39db8fecef13
Javier Francisco Dibo Gómez <jfdg0002@red.ujaen.es>
2024-12-16 20:08:36 +0100

    Creados tests pero no funcionan

--- src/test/java/clubSocios/servicios/TestControladorClub.java
+++ src/test/java/clubSocios/servicios/TestControladorClub.java
@@ -2,107 +2,100 @@ package clubSocios.servicios;
 
 import es.ujaen.dae.clubSocios.entidades.Temporada;
 import es.ujaen.dae.clubSocios.rest.dto.DTOTemporada;
-import es.ujaen.dae.clubSocios.rest.dto.Mapeador;
 import es.ujaen.dae.clubSocios.servicios.ServicioClub;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import es.ujaen.dae.clubSocios.excepciones.SocioNoExiste;
+import es.ujaen.dae.clubSocios.rest.dto.DTOSocio;
+import es.ujaen.dae.clubSocios.enums.EstadoCuota;
 
 import java.util.Objects;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
-@SpringBootTest(classes = es.ujaen.dae.clubSocios.app.Main.class)
+@SpringBootTest(classes = es.ujaen.dae.clubSocios.app.Main.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class TestControladorClub {
+
+    @LocalServerPort
+    int localPort;
 
     @Autowired
     private ServicioClub servicioClub;
 
-    // private Mapeador mapeador;
-
     private TestRestTemplate testRestTemplate;
+    private String baseUrl;
 
     @BeforeEach
     void setUp() {
-        testRestTemplate = new TestRestTemplate();
+        baseUrl = "http://localhost:" + localPort;
+        var restTemplateBuilder = new RestTemplateBuilder().rootUri(baseUrl);
+        testRestTemplate = new TestRestTemplate(restTemplateBuilder);
     }
 
-    // Tests a realizar:
-
-    /**
-     * 1. **Crear una temporada (admin)**
-     *
-     * - Endpoint: `/clubsocios/temporadas`
-     * - Método HTTP: POST
-     * - Autenticación: Requerida (admin)
-     * - Datos de entrada: DTOTemporada
-     */
     @Test
     @DirtiesContext
     void testCrearTemporada() {
-        // Datos de entrada
         DTOTemporada dtoTemporada = new DTOTemporada(null, 2025);
-        String base = "http://localhost:8080";
 
-        // Realizar la petición POST
         ResponseEntity<Void> response = testRestTemplate.withBasicAuth("direccion@clubsocios.es", "serviceSecret")
-                .postForEntity(base + "/clubsocios/temporadas", dtoTemporada, Void.class);
+                .postForEntity("/clubsocios/temporadas", dtoTemporada, Void.class);
 
-        // Verificar la respuesta
         assertEquals("respuesta", HttpStatus.CREATED, response.getStatusCode());
 
         // Intentar crear una temporada con el mismo año
         response = testRestTemplate.withBasicAuth("direccion@clubsocios.es", "serviceSecret")
-                .postForEntity(base + "/clubsocios/temporadas", dtoTemporada, Void.class);
+                .postForEntity("/clubsocios/temporadas", dtoTemporada, Void.class);
 
-        // Verificar la respuesta
         assertEquals("respuest", HttpStatus.CONFLICT, response.getStatusCode());
     }
 
-    /**
-     * 2. **Obtener una temporada (todos)**
-     *
-     * - Endpoint: `/clubsocios/temporadas/{anio}`
-     * - Método HTTP: GET
-     * - Autenticación: No requerida
-     * - Respuestas esperadas:
-     *      - 200 OK con el DTOTemporada si la temporada existe
-     *      - 404 Not Found si la temporada no existe
-     */
     @Test
     @DirtiesContext
     void testObtenerTemporada() {
         int anio = 2025;
 
-        String base = "http://localhost:8080";
-        // Crear una temporada para la prueba
-        servicioClub.crearTemporada(servicioClub.login("direccion@clubsocios.es", "serviceSecret").orElseThrow(SocioNoExiste::new), new Temporada(anio));
+        Temporada temporada = new Temporada(anio);
 
-        // Realizar la petición GET
-        ResponseEntity<DTOTemporada> response = testRestTemplate.getForEntity(base + "/clubsocios/temporadas/" + anio, DTOTemporada.class);
+        servicioClub.crearTemporada(
+                servicioClub.login("direccion@clubsocios.es", "serviceSecret")
+                        .orElseThrow(SocioNoExiste::new),
+                temporada
+        );
 
-        // Verificar la respuesta
+        ResponseEntity<DTOTemporada> response = testRestTemplate.getForEntity(baseUrl + "/clubsocios/temporadas/" + anio,
+                DTOTemporada.class
+        );
+
         assertEquals("status", HttpStatus.OK, response.getStatusCode());
         assertEquals("año", anio, Objects.requireNonNull(response.getBody()).anio());
-
-        // Intentar obtener una temporada que no existe
-        response = testRestTemplate.getForEntity(base + "/clubsocios/temporadas/2026", DTOTemporada.class);
-
-        // Verificar la respuesta
-        assertEquals("respuesta", HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
+    @Test
+    @DirtiesContext
+    void testCrearSocio() {
+        DTOSocio dtoSocio = new DTOSocio("prueba@gmail.com", "Pedro", "Apellido1", "12345678A", "690123456", "123456", EstadoCuota.PAGADA);
 
-    // 3. Crear un socio (todos)
+        ResponseEntity<Void> response = testRestTemplate.postForEntity(baseUrl + "/clubsocios/socios", dtoSocio, Void.class);
+
+        assertEquals("respuesta", HttpStatus.CREATED, response.getStatusCode());
+
+        // Intentar crear un socio con el mismo email
+        response = testRestTemplate.postForEntity(baseUrl + "/clubsocios/socios", dtoSocio, Void.class);
+
+        assertEquals("respuesta", HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
     // 4. Obtener un socio (todos)
     // 5. Crear una actividad (admin)
     // 6. Obtener actividades de una temporada (todos)
